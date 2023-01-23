188cfbb23bde2689746e7b153a75173afd147100
manitaloekemeijer <maanietaa@hotmail.com>
2023-01-23 21:30:35 +0100

    mijn eerste commit

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,35 +1,119 @@
-import React, { createContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, {createContext, useState, useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
+import axios from "axios";
+
+//zie https://www.npmjs.com/package/jwt-decode
+import jwt_decode from "jwt-decode";
 
 export const AuthContext = createContext({});
 
-function AuthContextProvider({ children }) {
-  const [isAuth, toggleIsAuth] = useState(false);
-  const navigate = useNavigate();
+function AuthContextProvider({children}) {
+    const [Auth, setAuth] = useState({
+        isAuthenticated: false,
+        user: null,
+        status: 'pending',
+    });
+    const navigate = useNavigate();
 
-  function login() {
-    console.log('Gebruiker is ingelogd!');
-    toggleIsAuth(true);
-    navigate('/profile');
-  }
+    useEffect(() => {
+        // haal de JWT op uit Local Storage
+        const storedToken = localStorage.getItem('token')
+        console.log(storedToken)
+        // als er WEL een token is, haal dan opnieuw de gebruikersdata op
+        if (storedToken && Math.floor(Date.now() / 1000) < decodedToken.exp){
+            console.log("de gebruiker is nog steeds ingelogd")
+            const decodedToken = jwt_decode(storedToken);
+            void fetchUserData(storedToken, decodedToken.sub)
 
-  function logout() {
-    console.log('Gebruiker is uitgelogd!');
-    toggleIsAuth(false);
-    navigate('/');
-  }
+            // } else {
+            //     console.log("De token is verlopen")
+            //     localStorage.removeItem('token')
+            // }
+        } else {
+            // als er GEEN token is doen we niks
+            setAuth({
+                ...Auth,
+                isAuth: false,
+                user: null,
+                status: "done"
+            })
+        }
+    }, [])
 
-  const contextData = {
-    isAuth: isAuth,
-    login: login,
-    logout: logout,
-  };
+// jwt als argument meegegeven aan login: regel 29 SignIn. Is response.data.accessToken
+    function login(JWT) {
 
-  return (
-    <AuthContext.Provider value={contextData}>
-      {children}
-    </AuthContext.Provider>
-  );
+        // console.log(`dit is console.log regel 18 AuthContext ${jwt}`)
+        console.log('Gebruiker is ingelogd!');
+
+        //zet token in de local storage bij inloggen
+        localStorage.setItem('storedToken', JWT);
+
+        //zie  https://www.npmjs.com/package/jwt-decode. Dit is overigens dezelfde info die je op kunt halen op jwt.io.
+        const decodedToken = jwt_decode(JWT);
+
+        // console.log(decodedToken);
+
+        //decodedToken.sub is de id uit regel 65
+        void fetchUserData(JWT, decodedToken.sub)
+        navigate('/profile');
+    }
+
+    async function fetchUserData(JWT, id) {
+        try {
+            const response = await axios.get(`http://localhost:3000/600/users/${id}`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${JWT}`,
+                }
+            })
+            setAuth({
+                isAuthenticated: true,
+                user: {
+
+                    //hier mag ook email: response.data.email,
+                    email: response.data.email,
+
+                    //hier mag ook id: response.data.id,
+                    id: response.data.sub,
+
+                    username: response.data.username
+                },
+                status: "done",
+            });
+
+            console.log(response)
+        } catch (e) {
+            console.log.error(e)
+        }
+    }
+
+    function logout() {
+        console.log('Gebruiker is uitgelogd!');
+        //Verwijdert alleen token (dit gebruiken als je ergens anders in je applicatie ook de local storage nog gaat gebruiken, anders worden die gegevens ook verwijderd, zoals bij regel 68)
+        localStorage.removeItem('jwtToken')
+        // localStorage.clear()
+        // verwijdert alle tokens uit local storage bij uitloggen
+        setAuth({
+            isAuthenticated: false,
+            user: null,
+            status: "done",
+        });
+        navigate('/');
+    }
+
+    const contextData = {
+        isAuth: Auth.isAuthenticated,
+        user: Auth.user,
+        login: login,
+        logout: logout,
+    };
+
+    return (
+        <AuthContext.Provider value={contextData}>
+            {children}
+        </AuthContext.Provider>
+    );
 }
 
 export default AuthContextProvider;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,19 +1,87 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useContext, useState} from 'react';
+import {Link} from 'react-router-dom';
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+import {AuthContext} from "../context/AuthContext";
 
 function SignUp() {
-  return (
-    <>
-      <h1>Registreren</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque consectetur, dolore eaque eligendi
-        harum, numquam, placeat quisquam repellat rerum suscipit ullam vitae. A ab ad assumenda, consequuntur deserunt
-        doloremque ea eveniet facere fuga illum in numquam quia reiciendis rem sequi tenetur veniam?</p>
-      <form>
-        <p>*Invoervelden*</p>
-      </form>
-      <p>Heb je al een account? Je kunt je <Link to="/signin">hier</Link> inloggen.</p>
-    </>
-  );
+    const [formState, setFormState] = useState({
+        email: '',
+        password: '',
+        username: '',
+    });
+    // const navigate = useNavigate();
+    const {login} = useContext( AuthContext )
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        console.log("De gebruiker is geregistreerd")
+        try {
+            const response = await axios.post('http://localhost:3000/register', {
+                email: formState.email,
+                password: formState.password,
+                username: formState.username,
+            })
+            // waarom werkt dit hier niet??? console.log(response);
+            // navigate('/signin');
+            login(response.data.accessToken)
+        } catch (e) {
+            console.error(e)
+        }
+        // dit hoorde bij stap 3: console.log(`console.log van handlechange ${formState.email} ${formState.password} ${formState.user}`)
+    }
+
+
+    function handleChange(e) {
+        const changedFieldName = e.target.name;
+
+        setFormState({
+            ...formState,
+            [changedFieldName]: e.target.value,
+        });
+    }
+
+    return (
+        <>
+            <h1>Registreren</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque consectetur, dolore eaque
+                eligendi
+                harum, numquam, placeat quisquam repellat rerum suscipit ullam vitae. A ab ad assumenda, consequuntur
+                deserunt
+                doloremque ea eveniet facere fuga illum in numquam quia reiciendis rem sequi tenetur veniam?</p>
+            <form onSubmit={handleSubmit}>
+                <label>emailadres:
+                    <input
+                        placeholder="Email"
+                        type="email"
+                        name="email"
+                        value={formState.email}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>password:
+                    <input
+                        placeholder="Password"
+                        type="password"
+                        name="password"
+                        value={formState.password}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>username:
+                    <input
+                        placeholder="Username"
+                        type="text"
+                        name="username"
+                        value={formState.username}
+                        onChange={handleChange}
+                    />
+                </label>
+                <button type="submit">Registreren</button>
+            </form>
+            <p>Heb je al een account? Je kunt je <Link to="/signin">hier</Link> inloggen.</p>
+        </>
+    );
 }
 
 export default SignUp;
